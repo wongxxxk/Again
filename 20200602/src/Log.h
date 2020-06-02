c60cd0e36723bb4e6d2ef43a8378522bebbb75fb
杭州大空翼 <47619149+wongxxxk@users.noreply.github.com>
2020-06-02 19:43:34 +0800

    20200602引用Over

--- 20200602/src/Log.h
+++ 20200602/src/Log.h
@@ -1,3 +1,10 @@
 #pragma once
+//预处理指令,只编译一次
+/*可以用以下替代
+#ifndef _LOG_H
+#define _LOG_H
+xxxx
+#endif
+*/
 
 void Log(const char* message);
--- 20200602/src/Main.cpp
+++ 20200602/src/Main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
-#include "Log.h"
+//#include "Log.h"
+//ALT+G 可看反汇编(从汇编层面优化)
+typedef char	I8;
+typedef short	I16;
+typedef int		I32;
+typedef float	f32;
+typedef unsigned short U16;
+typedef unsigned int   U32;
+
+#define Log(x) std::cout << x << std::endl
+
 
 int main()
 {
-	std::cout << "Hello world" << std::endl;
-	Log("Test");
+	I32 var = 0;
+	I32* ptr = &var;//c++指针大小和指向的地址类型无关,WIN32 4字节
+	*ptr = 100;
+
+	char* buffer = new char[8];//动态申请内存？heap
+	memset(buffer, 0, 8);
+	delete[] buffer;//释放掉
+
+	int& ref = var;// 引用不会真的创建ref变量，只是更方便≈ (int* ref = &var)
+	Log(var);
+
+	return 0;
 	std::cin.get();
 }
